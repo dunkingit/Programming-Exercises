e7972f338ee389171895fef9660ee90143536f97
Steven Rebuck <coolfire719@yahoo.com>
2023-09-27 11:01:42 -0400

    Finish uncommenting lines

--- Module1/13_Managing_Inheritance/exercise/src/main/java/com/techelevator/Wall.java
+++ Module1/13_Managing_Inheritance/exercise/src/main/java/com/techelevator/Wall.java
@@ -13,14 +13,6 @@ abstract class Wall {
         return color;
     }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getName() {
         return name;
     }
