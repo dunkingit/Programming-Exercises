0b2478500f99029728fa88cf933b957208be0b07
Rachelle Rauh <rachelle.rauh@techelevator.com>
2023-10-03 11:55:00 -0400

    File IO lecture final

--- Module1/17b_File_IO_Writing/lecture-final/src/main/java/com/techelevator/Lecture.java
+++ Module1/17b_File_IO_Writing/lecture-final/src/main/java/com/techelevator/Lecture.java
@@ -1,6 +1,7 @@
 package com.techelevator;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
@@ -20,8 +21,30 @@ public class Lecture {
 		 * A new instance of File can be created from a String that contains a file system path
 		 */
 
+		File file = new File("PeterPan.txt");
 
+		File mittensJournal = new File("mittenJournal.txt");
 
+		System.out.println(file.getAbsolutePath());
+		if (file.exists()) {
+			System.out.println("the file exists");
+		}
+
+		try (Scanner inputScanner = new Scanner(file); PrintWriter writer = new PrintWriter(mittensJournal)) {
+			int lineNumber = 1;
+			while (inputScanner.hasNextLine()) {
+				String currentLine = inputScanner.nextLine();
+				if (currentLine.contains("Hook")) {
+					writer.println(lineNumber + ") " + currentLine.replaceAll("Hook", "Glove"));
+					System.out.println(lineNumber + ") " + currentLine.replaceAll("Hook", "Mittens"));
+				}
+
+				lineNumber++;
+			}
+		} catch(FileNotFoundException e){
+			System.out.println(file.getName() + " was not found");
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
