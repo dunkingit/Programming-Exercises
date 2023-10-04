6183d8b56ce17fdc0025a0742633a2db6365ed51
Unbound Coding <coolfire719@yahoo.com>
2023-10-03 22:44:23 -0400

    Updating repo with part exercises done.

--- Module1/17b_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ Module1/17b_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,21 +1,57 @@
 package com.techelevator;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class FindAndReplace {
 
     // Use this scanner for all user input. Don't create additional Scanners with System.in
     private final Scanner userInput = new Scanner(System.in);
+    StringBuilder text = new StringBuilder();
+    public String input(String message){
+        System.out.println(message);
+        return userInput.nextLine();
+    }
 
     public static void main(String[] args) {
         FindAndReplace findAndReplace = new FindAndReplace();
         findAndReplace.run();
+
     }
 
     public void run() {
-        /* Your code goes here */
+        String promptInputSearchWord = input("What is the search word?");
+        String promptInputReplaceWord = input("What is the replacement word?");
+        String promptInputSourceFile = input("What is the source file?");
 
+        try (Scanner reader = new Scanner(new File(promptInputSourceFile))) {
+            while (reader.hasNextLine()) {
+                text.append(reader.nextLine()
+                        .trim().replaceAll(promptInputSearchWord,
+                                promptInputReplaceWord)).append("\r\n");
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Error: " + e.getMessage());
+            System.out.println("The source file could not be found. Exiting...");
+            System.exit(0);
+        }
+        String promptInputDestinationFile = input("What is the destination file? ");
+        if (promptInputSourceFile.equals(promptInputDestinationFile)) {
+            System.out.println("Source and destination files are the same. This might corrupt your data. Please provide a different destination.");
+            return;
+        }
 
+        try (PrintWriter print = new PrintWriter(promptInputDestinationFile)) {
+            print.println(text);
+            if (print.checkError()) {
+                System.out.println("There was an error writing to the destination file.");
+                System.exit(0);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Error: " + e.getMessage());
+            System.out.println("The destination file could not be accessed. Exiting...");
+            System.exit(0);
+        }
     }
-
 }
+
