7dec5baa53a5eb904fb7a8cb793157edbadc469e
Rachelle Rauh <rachelle.rauh@techelevator.com>
2023-10-04 12:55:21 -0400

    18_Review Final

---
+++ Module1/18_Review/lecture-final/src/main/java/com/techelevator/items/Apparel.java
@@ -0,0 +1,17 @@
+package com.techelevator.items;
+
+public class Apparel extends Item{
+
+    public Apparel(String sku) {
+        super(sku);
+    }
+
+    public String getDepartment(){
+        return "A";
+    }
+
+    @Override
+    public String toString() {
+        return "Apparel{}" + super.getName();
+    }
+}
--- Module1/18_Review/lecture-final/src/main/java/com/techelevator/items/filereader/InventoryBuilder.java
+++ Module1/18_Review/lecture-final/src/main/java/com/techelevator/items/filereader/InventoryBuilder.java
@@ -1,10 +1,18 @@
 package com.techelevator.items.filereader;
 
+import com.techelevator.items.Apparel;
+import com.techelevator.items.Food;
+import com.techelevator.items.HomeGood;
 import com.techelevator.items.Item;
+import com.techelevator.logger.Logger;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 
 public class InventoryBuilder {
@@ -15,10 +23,53 @@ public class InventoryBuilder {
         this.filePath = inventoryFilePath;
     }
 
+
     public Map<String, Item> getInventory(){
-        Map<String, Item> items = new HashMap<>();
-        items.put("test", new Item("test"));
-        items.put("test1", new Item("test1"));
-        return items;
+        Map<String, Item> inventoryMap = new HashMap<>();
+        //use the string filePath to make an instance of a File
+        File inventory = new File(filePath);
+        //build a Scanner object and use the File as a source
+        try(Scanner inventoryScanner = new Scanner(inventory)){
+            //look at each line in the file using the scanner
+            while(inventoryScanner.hasNext()){
+                //make new items
+                //based off of the current line from the file
+              String currentLineFromFile = inventoryScanner.nextLine();
+                //determine if you should build a Food, Apparel or HomeGood and put the item in the map
+               String[] currentLineArray =  currentLineFromFile.split("\\|");
+               String sku = currentLineArray[0];
+               String name = currentLineArray[1];
+               String description = currentLineArray[2];
+               String price = currentLineArray[3];
+               String dept = currentLineArray[4];
+
+               Item item = null;
+
+               if(dept.equals("A")){
+                   item = new Apparel(sku);
+
+               } else if(dept.equals("HG")){
+                   item =  new HomeGood(sku);
+
+               } else if(dept.equals("F")){
+                   item = new Food(sku,false);
+
+               } else {
+                   item = new Food(sku,true);
+              ;
+               }
+                item.setName(name);
+                item.setDescription(description);
+                item.setPrice(new BigDecimal(price));
+                inventoryMap.put(sku, item);
+            }
+
+        } catch (FileNotFoundException e) {
+            Logger logger = new Logger("log.txt");
+            logger.write(e.getMessage());
+        }
+
+
+        return inventoryMap;
     }
 }
