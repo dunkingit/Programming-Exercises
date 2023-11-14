f1da451fdd834d08412138ff5570809fdb753000
Steven <coolfire719@yahoo.com>
2023-11-14 10:32:48 -0500

    Finished with exercise homework modifying the dom

--- Module-3/07_DOM/exercise/shopping-list/js/shopping-list.js
+++ Module-3/07_DOM/exercise/shopping-list/js/shopping-list.js
@@ -2,22 +2,48 @@
 
 // add groceries
 
+let pageTitle = 'My Shopping List'
+
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function setPageTitle() {}
+function setPageTitle() {
+  document.getElementById('title').innerText = pageTitle
+}
 
+let groceries = populateGroceryList()
+
+function populateGroceryList(){
+  let temp = []
+for(let i = 1; i < 11; i++){
+  temp.push(`item ${i}`)
+}
+return temp}
+
+
+const currentTable = document.getElementById("groceries")
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() {
+  groceries.forEach((cu)  => addToTable(cu, currentTable))
+}
+
+function addToTable(item, tableName){
+  const changeItems = document.createElement("li");
+  changeItems.innerText = `${item}`;
+  tableName.insertAdjacentElement('beforeend', changeItems);
+}
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
  * to every list item and add the class completed to each one
  */
-function markCompleted() {}
+function markCompleted() {
+  let array = document.querySelectorAll("li")
+  array.forEach((cu) => cu.setAttribute("class", "completed") )
+}
 
 setPageTitle();
 
