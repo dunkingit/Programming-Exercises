3367db8020ad7e7e212ee9981c9402ff0fb7c20a
Rachelle Rauh <rachelle.rauh@techelevator.com>
2023-11-16 12:50:43 -0500

    js review lecture final

--- Module-3/10_JS_Review/lecture-final/pirate-game/js/app.js
+++ Module-3/10_JS_Review/lecture-final/pirate-game/js/app.js
@@ -7,36 +7,86 @@ let clock;
 
 // Creates the Grid -- this should only occur after the DOM loads
 
+document.addEventListener('DOMContentLoaded', ()=>{
+    createGrid();
+    
+    document.querySelector('body').addEventListener('keyup', (event)=>{
+        if(event.key === 'ArrowRight'){
+            moveShipRight();
+        }
+        if(event.key === 'ArrowLeft'){
+            moveShipLeft();
+        }
+        if(event.key === 'ArrowDown'){
+            moveShipDown();
+        }
+        if(event.key === 'ArrowUp'){
+            moveShipUp();
+        }
+    })
 
+    const resetBtnRef = document.getElementById('resetButton');
+    resetBtnRef.addEventListener('click', resetGame);
+})
 
 /*
   Move the Ship 
 */
 function moveShipRight() {
-
+    const ship = getShipLocation();
+    const rightNeighbor = ship.nextElementSibling;
+    if(rightNeighbor != null && !rightNeighbor.classList.contains('pirate')){
+        ship.classList.remove('boat');
+        rightNeighbor.classList.add('boat');
+    }
+   
 }
 
 function moveShipLeft() {
-
+    const ship = getShipLocation();
+    const leftNeighbor = ship.previousElementSibling;
+    if(leftNeighbor != null && !leftNeighbor.classList.contains('pirate')){
+        ship.classList.remove('boat');
+        leftNeighbor.classList.add('boat');
+    }
 }
 
 function moveShipDown() {
+    const ship = getShipLocation();
+    const shipCurrentIndex = Array.from(ship.parentNode.children).indexOf(ship);
+    //get ref to ship's parent's next element sibling
+    const nextRowFromShip = ship.parentNode.nextElementSibling;
+    if(nextRowFromShip != null){
+        const newLocation = nextRowFromShip.children[shipCurrentIndex];
+  
+        if(newLocation != null && !newLocation.classList.contains('pirate')){
+            ship.classList.remove('boat');
+            newLocation.classList.add('boat');
+        }
+    }
+   
 
 }
 
 function moveShipUp() {
-
+    const ship = getShipLocation();
+    const shipCurrentIndex = Array.from(ship.parentNode.children).indexOf(ship);
+    const previousRowFromShip = ship.parentNode.previousElementSibling;
+    if(previousRowFromShip != null){
+        const newLocation = previousRowFromShip.childNodes[shipCurrentIndex];
+        
+        if(newLocation != null && !newLocation.classList.contains('pirate')){
+            ship.classList.remove('boat');
+            newLocation.classList.add('boat');
+        }
+    }
 }
-
 
 
 function getShipLocation() {
     // Get the return the current location of the ship
- 
+    return document.getElementById('frame').querySelector('.boat')
 }
-
-
-
 
 
 /*
@@ -44,22 +94,25 @@ function getShipLocation() {
 */
 
 
-
-
-
 /*
     Loss Conditions
 */
-
-
-
 
 /**
  * Reset the Game
  */
 function resetGame() {
-
-    
+    if(getShipLocation() !== null){
+        const boat = getShipLocation();
+        boat.classList.remove('boat');
+    }
+    createObstacles();
+    const container = document.getElementById('frame');
+   
+    const startingCell = container.children[0].children[0];
+    startingCell.classList.add('boat');
+    const lastCell = container.children[9].children[9];
+    lastCell.classList.add('treasure');
 }
 
 
@@ -69,7 +122,12 @@ function resetGame() {
 /**
  * Creates the game grid with 10 rows appended to the element with id = 'main'
  */
-function createGrid(){}
+function createGrid(){
+    const frameRef = document.getElementById('frame');
+    for(let i = 0; i < 10; i++){
+        buildRow(frameRef)
+    }
+}
 
 
 /**
@@ -77,7 +135,16 @@ function createGrid(){}
  * then appends the created row to frame
  * @param {HTMLElement} frame 
  */
-function buildRow(frame) {}
+function buildRow(frame) {
+    const rowRef = document.createElement('div');
+    rowRef.setAttribute('class', 'row');
+
+    frame.appendChild(rowRef)
+
+    for(let i = 0; i < 10; i++){
+        buildSquare(rowRef);
+        }
+}
 
 
 
@@ -85,11 +152,28 @@ function buildRow(frame) {}
  * Builds the grid squares by creating a div and setting its class to square
  * then appends to the end of the given row
  * @param {HTMLElement} row 
- * @param {int} count 
+ * 
  */
-function buildSquare(row, count) {}
+function buildSquare(row) {
+    const sqrRef = document.createElement('div');
+    sqrRef.setAttribute('class', 'square');
+    row.appendChild(sqrRef)
+}
 
-
+function createObstacles(){
+    const rows = document.querySelectorAll('.row');
+    const rowsArray = Array.from(rows);
+    rowsArray.forEach((currentRow, rowIndex)=>{
+       const cells = currentRow.querySelectorAll('.square');
+       const cellsArray = Array.from(cells);
+       cellsArray.forEach((cell, cellIndex)=>{
+        if((cellIndex !== 0 && rowIndex !== 0) && (cellIndex !== 9 && rowIndex !== 9)){
+            addObstacles(cell)
+        }
+        
+       })
+    })
+}
 
 
 /**
@@ -99,16 +183,18 @@ function buildSquare(row, count) {}
  */
 function addObstacles(cell) {
     // remove any existing pirates or icebergs
-
+    cell.classList.remove('pirate');
+    cell.classList.remove('iceberg');
 
     const rand = getRandomNumber(100, false);
 
-    if (rand > 85) {
-        // Add iceberg here
- 
-    } else if (rand > 80) {
+    // if (rand > 85) {
+    //     // Add iceberg here
+    //     cell.classList.add('iceberg')
+    // } else 
+    if (rand > 80) {
         // Add pirates here
-
+        cell.classList.add('pirate')
     } 
 }
 
