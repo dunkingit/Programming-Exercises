ab82def4391e5af3c27e8347694106afced73ce8
Rachelle Rauh <rachelle.rauh@techelevator.com>
2023-11-17 09:35:13 -0500

    intro to vue lecture

--- Module-3/10_JS_Review/lecture-final/quiz/quiz-client/quiz.js
+++ Module-3/10_JS_Review/lecture-final/quiz/quiz-client/quiz.js
@@ -1,10 +1,12 @@
+
 function fetchRandomQuestion(){
     fetch('http://localhost:8080/questions/random')
     .then(response => response.json())
     .then(data =>{
-        console.log(data)
+       
         const container = document.getElementById('container');
         const pTag = container.querySelector('p');
+
         pTag.innerText = data.question;
     })
 }
