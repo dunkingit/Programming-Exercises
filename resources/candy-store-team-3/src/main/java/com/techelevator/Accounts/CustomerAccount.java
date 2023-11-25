a5fbac58231b0944fe542e9e353fc882992bb2f8
Steven <coolfire719@yahoo.com>
2023-11-25 14:40:37 -0500

    sorting

--- resources/candy-store-team-3/src/main/java/com/techelevator/Accounts/CustomerAccount.java
+++
@@ -1,9 +0,0 @@
-package com.techelevator.Accounts;
-
-import com.techelevator.filereader.LogFileWriter;
-
-public class CustomerAccount extends Account{
-    public CustomerAccount(LogFileWriter accountLogger) {
-        super(accountLogger);
-    }
-}
--- resources/candy-store-team-3/src/main/java/com/techelevator/Accounts/VenderAccount.java
+++
@@ -1,9 +0,0 @@
-package com.techelevator.Accounts;
-
-import com.techelevator.filereader.LogFileWriter;
-
-public class VenderAccount extends Account {
-    public VenderAccount(LogFileWriter accountLogger) {
-        super(accountLogger);
-    }
-}
